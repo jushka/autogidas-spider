5393d8bfd20f8e3609453a08bdc36e7df43f15fe
jushka <tautrimas25@gmail.com>
2020-08-11 23:30:52 +0300

    clean scraped data

--- autogidas_spider/pipelines.py
+++ autogidas_spider/pipelines.py
@@ -10,5 +10,14 @@ from itemadapter import ItemAdapter
 
 class AutogidasSpiderPipeline:
     def process_item(self, item, spider):
-        # item['make_model'] = 'renamed'
+        item['make'] = item['make'].strip()
+        item['model'] = item['model'].strip()
+        item['fuel_type'] = item['fuel_type'].strip()
+        item['body_type'] = item['body_type'].strip()
+        item['gearbox_type'] = item['gearbox_type'].strip()
+        item['price'] = item['price'].replace(' €', '').replace(' ', '')
+        item['mfd_date'] = item['mfd_date'][:4]
+        if item['mileage'] is not None:
+            item['mileage'] = item['mileage'].replace(' km', '')
+
         return item
