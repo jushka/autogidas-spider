70387257607b8824d93a802dfece44d2d8936bff
jushka <tautrimas25@gmail.com>
2020-08-11 23:41:39 +0300

    some minor tweaks

--- autogidas_spider/spiders/autogidas.py
+++ autogidas_spider/spiders/autogidas.py
@@ -2,14 +2,9 @@ import scrapy
 
 class AutogidasSpider(scrapy.Spider):
   name = 'autogidas'
-
-  def start_requests(self):
-    # urls = ['https://autogidas.lt/skelbimai/automobiliai/?f_1%5B0%5D=&f_model_14%5B0%5D=&f_215=&f_216=&f_41=&f_42=&f_376='] #all ads
-    # urls = ['https://autogidas.lt/skelbimai/automobiliai/?f_1%5B0%5D=Alfa+Romeo&f_model_14%5B0%5D=&f_215=&f_216=&f_41=&f_42=&f_376='] #alfa romeo: 24 ads
-    urls = ['https://autogidas.lt/skelbimai/automobiliai/?f_1%5B0%5D=BMW&f_model_14%5B0%5D=Serija+7&f_215=&f_216=&f_41=&f_42=&f_376='] #bmw 7 series: 74 ads
-    # urls = ['https://autogidas.lt/skelbimai/automobiliai/?f_1%5B0%5D=BMW&f_model_14%5B0%5D=&f_215=&f_216=&f_41=&f_42=&f_376='] #bmw: 1061 ads
-    for url in urls:
-      yield scrapy.Request(url=url, callback=self.parse)
+  start_urls = [
+    'https://autogidas.lt/skelbimai/automobiliai/?f_1%5B0%5D=BMW&f_model_14%5B0%5D=Serija+7&f_215=&f_216=&f_41=&f_42=&f_376='
+  ]
 
   def parse(self, response):
     ads_links = response.css('article.list-item > a::attr(href)').getall()
@@ -20,11 +15,11 @@ class AutogidasSpider(scrapy.Spider):
       yield response.follow(next_page, self.parse)
 
   def parse_ad(self, response):
-    params_block = response.xpath('//div[@class="params-block"][2]')
-    keys = params_block.xpath('.//div[@class="left"]/text()').getall()
-    values = params_block.xpath('.//div[@class="right"]/text()').getall()
+    params_blocks = response.xpath('//div[@class="params-block"]')
+    keys = params_blocks[1].xpath('.//div[@class="left"]/text()').getall()
+    values = params_blocks[1].xpath('.//div[@class="right"]/text()').getall()
     params = dict(zip(keys, values))
-    price = response.xpath('//div[@class="params-block"][1]//div[@class="price"]/text()').get()
+    price = params_blocks[0].xpath('.//div[@class="price"]/text()').get()
     img_url = response.xpath('//div[@class="big-photo"]/img/@src').get()
 
     yield {
